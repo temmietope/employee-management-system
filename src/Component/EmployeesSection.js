20283bb5500b88f34e0e3424476c3bf8894e7894
Temitope <temmieayodele@gmail.com>
2019-07-26 15:30:35 +0100

    added a render function

---
+++ src/Component/EmployeesSection.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import SideBar from "./SideBar";
+import EmployeesInfo from "./EmployeesInfo";
+
+const EmployeesSection = () => {
+  const [employees, setEmployee] = useState([]);
+  useEffect(() => {
+    async function fetchUsers() {
+      const res = await fetch(
+        "https://api.github.com/search/users?q=location%3Anigeria&per_page=7"
+      );
+      const data = await res.json();
+      return data.items;
+    }
+    fetchUsers().then(res => setEmployee(res));
+  }, []);
+
+  const renderEmployeeSection = () => {
+    return (
+      <div>
+        {console.log(employees)}
+        <section>
+          <SideBar />
+        </section>
+        <section>
+          <EmployeesInfo employees={employees} />
+        </section>
+      </div>
+    );
+  };
+  const renderLoading = () => {
+    return <div>Loading...</div>;
+  };
+  return (
+    <>{employees.length > 0 ? renderEmployeeSection() : renderLoading()}</>
+  );
+};
+
+export default EmployeesSection;
