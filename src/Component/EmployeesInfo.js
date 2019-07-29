4c3440c0d30ab1a4061d4bba308e0095fb2e9e8a
Temitope <temmieayodele@gmail.com>
2019-07-29 20:41:16 +0100

    added new functions and a new component

--- src/Component/EmployeesInfo.js
+++ src/Component/EmployeesInfo.js
@@ -1,62 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { jobtitle, salary, status, time, duration } from "../utils/constants";
+import ModalForm from "./Modal";
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
 const EmployeesInfo = ({ employees, deleteEntry }) => {
-  const [staff, setStaff] = useState({
-    jobtitle: [
-      "Software Engineer",
-      "Software Engineer",
-      "Project Manager",
-      "Support Manager",
-      "Dev Ops",
-      "Content Developer",
-      "Information Analyst"
-    ],
-    salary: ["8,000", "7,500", "3,200", "5,000", "4,000", "2,500", "4,000"],
-    status: [
-      "worker",
-      "worker",
-      "worker",
-      "test period",
-      "test period",
-      "worker"
-    ],
-    duration: [
-      "1 year",
-      "2 years",
-      "1.5 years",
-      "7 months",
-      "3months",
-      "1 year",
-      "2 years"
-    ],
-    time: [
-      "full-time",
-      "full-time",
-      "part-time",
-      "part-time",
-      "full-time",
-      "full-time",
-      "part-time"
-    ]
-  });
+  //states
+  let [allEmployeesPortfolio, setAllemployeesPortfolio] = useState([]);
+  const [portfolio, setPorfolio] = useState({});
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  useEffect(() => {
-    console.log(staff);
-  });
 
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)"
-    }
-  };
+  //functions
   const openModal = () => {
     setModalIsOpen(true);
   };
@@ -65,6 +20,7 @@ const EmployeesInfo = ({ employees, deleteEntry }) => {
     setModalIsOpen(false);
   };
 
+  //renderFunction
   const renderEmployeesInfo = () => {
     return (
       <div className="employees-info">
@@ -78,24 +34,34 @@ const EmployeesInfo = ({ employees, deleteEntry }) => {
           <span>MANAGE</span>
         </div>
         {employees.map((employee, index) => {
+          const employeePortfolio = {
+            name:employee.login,
+            jobtitle: jobtitle[index],
+            salary: salary[index],
+            time: time[index],
+            status: status[index],
+            duration: duration[index]
+          };
+          allEmployeesPortfolio = [...allEmployeesPortfolio, employeePortfolio];
           return (
             <div key={index} className="employee">
               <input type="checkbox" name="employee" className="checkbox" />
               <span className="employee-name">
                 <img src={employee.avatar_url} alt="avatar" />
                 {`${employee.login}
-              ${staff.jobtitle[index]}`}
+              ${employeePortfolio.jobtitle}`}
               </span>
-              <span>{`${staff.salary[index]} GBP
-              ${staff.time[index]}
+              <span>{`${employeePortfolio.salary} GBP
+              ${employeePortfolio.time}
               `}</span>
-              <span>{`${staff.status[index]}
-              ${staff.duration[index]}`}</span>
+              <span>{`${employeePortfolio.status}
+              ${employeePortfolio.duration}`}</span>
               <span>
                 <button>
                   <i
                     className="fas fa-pencil-alt"
                     onClick={() => {
+                      setPorfolio(employeePortfolio);
                       openModal();
                     }}
                   />
@@ -114,44 +80,13 @@ const EmployeesInfo = ({ employees, deleteEntry }) => {
           );
         })}
         <Modal
+          className="modal"
           isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
           contentLabel="Example Modal"
         >
-          <h2>Edit Employee Info</h2>
-          <button onClick={closeModal}>Save</button>
-          <form>
-            <div>
-              <label>Name</label>
-              <input type="text" name="login" />
-            </div>
-            <div>
-              <label>JobTitle</label>
-              <input type="text" name="jobtitle" />
-            </div>
-            <div>
-              <label>Salary</label>
-              <input type="text" name="salary" /> GBP
-            </div>
-            <div>
-              <label>Full-time/parttime</label>
-              <input type="text" name="time" />
-            </div>
-            <div>
-              <label>Status</label>
-              <input type="text" name="status" />
-            </div>
-            <div>
-              <label>Duration</label>
-              <input type="text" name="duration" />
-            </div>
-
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
+          
+          <ModalForm details={portfolio} 
+          onRequestClose={closeModal}/>
         </Modal>
       </div>
     );
