95608d8d1ec8ef56a7b1d402bf2fb656add5fc9b
Temitope <temmieayodele@gmail.com>
2019-08-02 17:05:30 +0100

    formatted code

--- src/Component/SideBar.js
+++ src/Component/SideBar.js
@@ -2,17 +2,46 @@ import React from "react";
 
 const SideBar = () => {
   return (
-    <div className="sidebar">
+    <div className="side-bar">
       <div>
         <span>All Employees</span>
       </div>
       <div>
+        <h4>Project</h4>
         <ul>
-          <li>Aroma Sport</li>
-          <li>DSV</li>
-          <li>SeaFood Mall</li>
-          <li>FiveStar</li>
-          <li>Zeto Bank</li>
+          <li>
+            <i className="fab fa-atlassian" />
+            Aroma Sport
+          </li>
+          <li>
+            <i className="fab fa-dochub" />
+            DSV
+          </li>
+          <li>
+            <i className="fas fa-puzzle-piece" />
+            SeaFood Mall
+          </li>
+          <li>
+            <i className="fas fa-otter" />
+            FiveStar
+          </li>
+          <li>
+            <i className="fas fa-hat-wizard" />
+            Zeto Bank
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Status</h4>
+        <ul>
+          <li>
+            <i className="fas fa-battery-full" />
+            Full-time
+          </li>
+          <li>
+            <i className="fas fa-battery-half" />
+            Part-time
+          </li>
         </ul>
       </div>
     </div>
