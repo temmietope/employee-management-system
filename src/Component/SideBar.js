e58b050290ba2de035e6e24a4ace360b9edc594e
Temitope <temmieayodele@gmail.com>
2019-08-05 20:36:52 +0100

    added new functions

--- src/Component/SideBar.js
+++ src/Component/SideBar.js
@@ -1,46 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SideBar = () => {
+  const [lastClicked, setLastClicked] = useState(false);
+
+  const resetBackgroundColor = () => {
+    const list = document.querySelectorAll("li");
+    list.forEach(list => {
+      list.style.backgroundColor = "inherit";
+    });
+  };
+  const onClick = e => {
+    if (!lastClicked) {
+      e.target.style.backgroundColor = "rgba(189, 169, 169, 0.856)";
+      setLastClicked(true);
+    }
+    if (lastClicked) {
+      resetBackgroundColor();
+      e.target.style.backgroundColor = "rgba(189, 169, 169, 0.856)";
+    }
+  };
+
   return (
     <div className="side-bar">
       <div>
-        <span>All Employees</span>
+        <span>
+          {" "}
+          <i
+            className="far fa-check-circle"
+            style={{ color: "red", fontSize: "33px" }}
+          />
+          All Employees
+        </span>
       </div>
       <div>
         <h4>Project</h4>
         <ul>
-          <li>
-            <i className="fab fa-atlassian" />
-            Aroma Sport
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i className="fab fa-atlassian" style={{ color: "forestgreen" }} />
+            <span>Aroma Sport</span>
           </li>
-          <li>
-            <i className="fab fa-dochub" />
-            DSV
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i className="fab fa-dochub" style={{ color: "darkmagenta" }} />
+            <span>DSV</span>
           </li>
-          <li>
-            <i className="fas fa-puzzle-piece" />
-            SeaFood Mall
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i
+              className="fas fa-puzzle-piece"
+              style={{ color: "midnightblue" }}
+            />
+            <span>SeaFood Mall</span>
           </li>
-          <li>
-            <i className="fas fa-otter" />
-            FiveStar
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i className="fas fa-otter" style={{ color: "olivedrab" }} />
+            <span>FiveStar</span>
           </li>
-          <li>
-            <i className="fas fa-hat-wizard" />
-            Zeto Bank
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i className="fas fa-hat-wizard" style={{ color: "saddlebrown" }} />
+            <span>Zeto Bank</span>
           </li>
         </ul>
       </div>
       <div>
         <h4>Status</h4>
         <ul>
-          <li>
-            <i className="fas fa-battery-full" />
-            Full-time
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i className="fas fa-battery-full" style={{ color: "maroon" }} />
+            <span>Full-time</span>
           </li>
-          <li>
-            <i className="fas fa-battery-half" />
-            Part-time
+          <li
+            onClick={e => {
+              onClick(e);
+            }}
+          >
+            <i className="fas fa-battery-half" style={{ color: "orangered" }} />
+            <span>Part-time</span>
           </li>
         </ul>
       </div>
