b6cae055410036f7b50a59d90da001cc5b196bc9
Temitope <temmieayodele@gmail.com>
2020-05-13 22:29:33 +0100

    toggle sidebar

--- src/Component/EmployeesSection/index.js
+++ src/Component/EmployeesSection/index.js
@@ -1,25 +1,13 @@
 import React from "react";
 import SideBar from "../SideBar/index";
 import EmployeesManagement from "../EmployeesManagement/index";
-import "./EmployeesSection.css"
+import "./EmployeesSection.css";
 
 const EmployeesSection = () => {
-  const renderEmployeeSection = () => {
-    return (
-      <div className="employees-section">
-        <section>
-          <SideBar />
-        </section>
-        <section>
-          <EmployeesManagement />
-        </section>
-      </div>
-    );
-  };
   return (
-    <>
-      {renderEmployeeSection()}
-    </>
+    <div className="employees-section">
+      <EmployeesManagement />
+    </div>
   );
 };
 
