67c91e843083f767193d03c146a0cd6bd5403a8a
Temitope <temmieayodele@gmail.com>
2020-05-15 00:29:49 +0100

    css

--- src/Component/EmployeesManagement/Header.js
+++ src/Component/EmployeesManagement/Header.js
@@ -1,26 +1,26 @@
-import React from "react"
+import React from "react";
 
 const Header = () => {
-    return (
-        <header className="employee employee-header">
-            <section className="checkbox">
-                <input type="checkbox" className="checkbox" />
-            </section>
-            <section className="avatar" />
-            <section className="employee-name">
-                <span>EMPLOYEES</span>
-            </section>
-            <section className="employee-salary">
-                <span>SALARY</span>
-            </section>
-            <section className="employee-status">
-                <span>STATUS</span>
-            </section>
-            <section className="manage">
-                <span>MANAGE</span>
-            </section>
-        </header>
-    )
-}
+  return (
+    <header className="employee employee-header">
+      <section className="checkbox header_">
+        <input type="checkbox" className="checkbox" />
+      </section>
+      <section className="avatar header_" />
+      <section className="employee-name header_">
+        <span>EMPLOYEES</span>
+      </section>
+      <section className="employee-salary header_">
+        <span>SALARY</span>
+      </section>
+      <section className="employee-status header_">
+        <span>STATUS</span>
+      </section>
+      <section className="manage header_">
+        <span>MANAGE</span>
+      </section>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
